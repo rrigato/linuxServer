22aaac909f9ea74e1d6f9faa1451b38672c283c0
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2017-03-19 14:16:13 -0500

    Update autoStartup.sh
    Making the shell file start and stop the computer automatically

--- autoStartup.sh
+++ autoStartup.sh
@@ -1,16 +1,10 @@
-#Some computers do not support automatic startup in there BIOS/UEFI boot loader, you need to first check whether the computer
-#you are using supports it with the following command
 
-```
-sudo sh -c "echo 0 > /sys/class/rtc/rtc0/wakealarm"
-sudo sh -c "echo `date '+%s' -d '+ 3 minutes'` > /sys/class/rtc/rtc0/wakealarm"
-cat /sys/class/rtc/rtc0/wakealarm
-```
 
-#The last command will print the number of seconds since the epoch that you have the wakealarm set to go off on
+The next step is to create a shell file with the following information
 
-#To check if the wakealarm is supported view if the alrm_time is 5 minutes from now and the alrm_date is today
-```
-cat /proc/driver/rtc
-```
-#If it does then your computer supports automatic wake times
+#Sets the wakealarm for X hours after the computer shutsdown
+sh -c "echo 0 > /sys/class/rtc/rtc0/wakealarm" 
+sh -c "echo `date '+%s' -d '+ 5 minutes'` > /sys/class/rtc/rtc0/wakealarm" 
+
+#shuts the computer down now
+shutdown -h now
